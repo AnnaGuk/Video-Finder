222949a3c6b09fd8b144940293bd9e6a96499e3d
Anna Guk <guk.anna1989@gmail.com>
2019-03-31 16:19:25 +0200

    final changes

--- src/components/Show/Show.js
+++ src/components/Show/Show.js
@@ -3,27 +3,38 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Wrapper = styled(Link)`
-  width: 32%;
+  width: 100%;
+  max-width: 62.5rem;
   text-decoration: none;
   display: block;
   clear: both;
-  padding: 5px;
+  padding: 0.3125rem;
+  margin: 0.3125rem;
+  border: 2px solid rgb(105, 200, 161);
+  border-radius: 0.25rem;
+  height: 19.375rem;
 `;
 
 const Image = styled.img`
+  padding: 0.3125rem;
   width: 50%;
   float: left;
+  max-width: 12.5rem;
+`;
+
+const Description = styled.section`
+  color: rgb(46, 139, 87);
 `;
 
 const Show = ({ imdbID, poster, title, year, description }) => {
   return (
     <Wrapper to={`/details/${imdbID}`}>
       <Image src={`img/${poster}`} alt={`${title} cover`} />
-      <div>
-        <h3>{title}</h3>
-        <h2>{year}</h2>
+      <Description>
+        <h2>{title}</h2>
+        <h3>{year}</h3>
         <p>{description}</p>
-      </div>
+      </Description>
     </Wrapper>
   );
 };
--- src/components/ShowDetails/ShowDetails.js
+++ src/components/ShowDetails/ShowDetails.js
@@ -1,5 +1,13 @@
 import React, { Component } from "react";
 import Loader from "../Loader/Loader";
+import styled from "styled-components";
+
+const Description = styled.section`
+  color: rgb(46, 139, 87);
+  padding: 0.3125rem;
+  margin: 0.3125rem;
+  text-align: center;
+`;
 
 class ShowDetails extends Component {
   state = {
@@ -10,26 +18,27 @@ class ShowDetails extends Component {
     fetch(`http://localhost:3002/${this.props.show.imdbID}`)
       .then(r => r.json())
       .then(resp => this.setState({ rating: resp }))
-      .catch(() => this.setState({rating: 'No imdb data avalaible.'}));
+      .catch(() => this.setState({ rating: "No imdb data avalaible." }));
   }
 
   render() {
     return (
-      <section>
+      <Description>
         <h1>{this.props.show.title}</h1>
         <h2>{this.props.show.year}</h2>
         {this.state.rating ? <h5>{this.state.rating}</h5> : <Loader />}
-
         <p>{this.props.show.description}</p>
         <iframe
           src={`https://www.youtube-nocookie.com/embed/${
             this.props.show.trailer
           }`}
           frameBorder="0"
+          width="600"
+          height="400"
           allowFullScreen
           title={`Trailer for ${this.props.show.title}`}
         />
-      </section>
+      </Description>
     );
   }
 }
